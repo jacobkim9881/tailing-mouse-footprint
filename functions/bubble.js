cb8154669872f349ea9b977e51458b0c78084ae0
jacobkim9881 <jacobkim9881@gmail.com>
2020-12-14 18:15:57 +0900

    bug: draging issue fixed

--- functions/bubble.js
+++ functions/bubble.js
@@ -4,7 +4,7 @@ function mouseEvent(e) {
   
   let ran10 = Math.trunc(Math.random() * 10) + 5;
     let ranXpos = Math.trunc(Math.random() * ran10) + parseInt(e.clientX, 10);
-    let ranYpos = Math.trunc(Math.random() * 10) + parseInt(e.clientY, 10);
+    let ranYpos = Math.trunc(Math.random() * -10 - 10) + parseInt(e.clientY, 10);
     let ran20 = ranYpos - 30;
     obj.style.position = 'fixed';
     obj.style.backgroundImage = `linear-gradient(to bottom right, hsl(170, 100%, 50%), hsl(170, 100%, 0%)`;
