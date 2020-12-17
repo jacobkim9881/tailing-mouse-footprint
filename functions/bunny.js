58a52c902a385d694aba6af9ec357aab4c996ecf
jacobkim9881 <jacobkim9881@gmail.com>
2020-12-17 22:25:15 +0900

    refactor: making prototypes

--- functions/bunny.js
+++ functions/bunny.js
@@ -1,10 +1,12 @@
 //bunny.js
 localStorage.mouseObjArr = ['face', 'eye1', 'eye2',
            'ear1', 'ear2', 'ear3', 'ear4',
-           'mouth', 'mouth1', 'mouth2', 'mouth3',
-           'tie', 'tie1', 'tie2', 'tie3', 'tie4', 
+           'mouth', 'mouth1', 'mouth2', 'mouth3', 'mouth4', 'mouth5', 'mouth6', 'mouth7',
+           'tie', 'tie1', 'tie2',
+	   'blush', 'blush1', 
 	   'emotion', 'emotion1', 'emotion2',
-           'emotion3', 'emotion4', 'emotion5', 'emotion6'];
+           'emotion3', 'emotion4', 'emotion5', 
+	   'emotion6', 'emotion7', 'emotion8'];
 
 function createObjs() {
 let grpDivs = document.createElement('div');
@@ -26,172 +28,274 @@ function deleteObjs() {
 
 }
 
-function stylingEye(ele, pos) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 140) + 'px';
-ele.style.width = '10px';
-ele.style.height = '10px';
-ele.style.borderRadius = '50%';
-ele.style.backgroundColor = `hsl(0, 100%, 0%)`;
-ele.style.border = `2px solid black`;
-ele.style.zIndex = '1';
-/*
-ele.animate([
-  {left: (x + pos) + 'px'},
-  {left: (x + pos + 5) + 'px'},
-  {left: (x + pos) + 'px'}
-], {duration: 1000, iterations: Infinity})
-*/
+function objProto({element, position, left, top, width, height, borderRadius, zIndex, backgroundColor:{h,s,l}, transform}) {
+element.style.position = position;
+element.style.left = left + 'px';
+element.style.top = top + 'px';
+element.style.width = width +  'px';
+element.style.height = height + 'px';
+element.style.borderRadius = borderRadius;
+element.style.zIndex = zIndex;
+element.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
+element.style.transform = transform;
+
 }
 
-function stylingEar(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 20) + 'px';
-ele.style.width = '35px';
-ele.style.height = '100px';
-ele.style.borderRadius = '100% 30% 50% 30%';
-ele.style.backgroundColor = `hsl(0, 100%, 85%)`;
-//ele.style.border = `2px solid black`;
-ele.style.zIndex = '0';
-ele.style.transform = `skew(${howMuch}deg)`
+function getId(id) {
+  return document.getElementById(id);
+}
 
-if (howMuch) {
-ele.animate([
-  {borderRadius: '100% 30% 50% 30%' },
+function stylingFace() {
+const config = {
+	element: getId('face'),
+	position: 'fixed',
+	left: 100,
+	top: 100,
+	width: 100,
+	height: 100,
+	borderRadius: 30 + 'px',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 85},
+	transform: 'none'	
+
+}
+  objProto(config);
+
+}
+
+function stylingEye({id, left}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 140,
+	width: 15,
+	height: 15,
+	borderRadius: 50 + 'px',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 0},
+	transform: 'none'	
+}
+objProto(config);
+getId('eye1').style.border = '2px solid black';
+
+}
+
+function stylingEar({id, left, skewness}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 5,
+	width: 50,
+	height: 100,
+	borderRadius: '',
+	zIndex: 0,
+	backgroundColor: {h: 0, s: 100, l: 85},
+	transform: `skew(${skewness}deg)`
+}
+  objProto(config);
+  getId(id).animate([
+  {borderRadius: '100% 80% 50% 30%' },
   {borderRadius: '100% 0% 0% 10%' },
-  {borderRadius: '100% 30% 50% 30%' }], {duration:1000, iterations: Infinity})
-}
-}
+  {borderRadius: '100% 80% 50% 30%' }], {duration:1000, iterations: Infinity})
 
-
-function stylingEar2(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 55) + 'px';
-ele.style.borderRadius = '100% 30% 50% 30%';
-ele.style.width = '20px';
-ele.style.height = '40px';
-
-ele.style.backgroundColor = 'hsl(305, 100%, 84%)'
-ele.style.zIndex = '1';
-ele.style.transform = `skew(${howMuch}deg)`
 }
 
-function stylingMouth(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 180) + 'px';
-ele.style.width = '10px';
-ele.style.height = '10px';
-ele.style.backgroundColor = 'hsl(0, 100%, 50%, 0)',
-ele.style.borderTop = `2px solid black`;
-ele.style.borderLeft = `2px solid black`;
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
+function stylingEarHole({id, left, skewness}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 35,
+	width: 20,
+	height: 60,
+	borderRadius: '100% 30% 50% 30%',
+	zIndex: 1,
+	backgroundColor: {h: 305, s: 100, l: 84},
+	transform: `skew(${skewness}deg)`
+}
+objProto(config);
 }
 
-function stylingMouth1(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 170) + 'px';
-ele.style.width = '10px';
-ele.style.height = '10px';
-ele.style.borderLeft = `2px solid black`;
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
+function stylingMouth({id, left, rotate}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 180,
+	width: 10,
+	height: 10,
+	borderRadius: '',
+	zIndex: 2,
+	backgroundColor: '',
+	transform: `rotate(${rotate}deg)`
+}
+objProto(config);
+
+getId(id).style.borderTop = `2px solid black`;
+getId(id).style.borderLeft = `2px solid black`;
+
 }
 
-function stylingMouth2(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 160) + 'px';
-ele.style.width = '10px';
-ele.style.height = '10px';
-ele.style.borderRadius = '100% 30% 30% 100%';
-ele.style.borderTop = `2px solid black`;
-ele.style.borderLeft = `2px solid black`;
+function stylingMouth1({id, left}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 170,
+	width: 10,
+	height: 10,
+	borderRadius: '',
+	zIndex: 1,
+	backgroundColor: '',
+	transform: ``
+}
+objProto(config);
+getId(id).style.borderLeft = `2px solid black`;
 
-ele.style.backgroundColor = 'hsl(0, 100%, 0%)'
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
 }
 
-function stylingMouth3(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 155) + 'px';
-ele.style.width = '15px';
-ele.style.height = '10px';
+function stylingMouth2({id, left, rotate}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 160,
+	width: 13,
+	height: 13,
+	borderRadius: '100% 30% 30% 100%',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 0},
+	transform: `rotate(${rotate}deg)`
+}
+objProto(config);
+getId(id).style.borderTop = `2px solid black`;
+getId(id).style.borderLeft = `2px solid black`;
 
-ele.style.backgroundColor = 'hsl(0, 100%, 85%)'
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
 }
 
-function stylingtie(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 207) + 'px';
-ele.style.width = '20px';
-ele.style.height = '20px';
-ele.style.borderTop = `2px solid black`;
-ele.style.borderLeft = `2px solid black`;
-ele.style.borderRadius = `30%`
-
-ele.style.backgroundColor = 'hsl(0, 100%, 0%)'
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
+function stylingMouth3({id, left, rotate}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 155,
+	width: 20,
+	height: 10,
+	borderRadius: '',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 85},
+	transform: `rotate(${rotate}deg)`
 }
-function stylingblush(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 160) + 'px';
-ele.style.borderRadius = '50%'
-ele.style.width = '20px';
-ele.style.height = '10px';
-
-ele.style.backgroundColor = 'hsl(305, 100%, 70%)'
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
+objProto(config);
 }
 
-function stylingtie3(ele, pos, howMuch) {
-ele.style.position = 'fixed';
-ele.style.left = (x + pos) + 'px';
-ele.style.top = (y + 210) + 'px';
-ele.style.width = '13px';
-ele.style.height = '13px';
-ele.style.borderRadius = `100%`
+function stylingSmile({id, left, top, width, height, skewness}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: top,
+	width: width,
+	height: height,
+	borderRadius: '50%',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 50},
+	transform: `skewY(${skewness}deg)`
+}
+objProto(config);
 
-ele.style.backgroundColor = 'hsl(0, 100%, 0%)'
-ele.style.zIndex = '1';
-ele.style.transform = `rotate(${howMuch}deg)`
 }
 
-function addHearts(ele, pos, howMuch, ypos, size) {
-ele.style.position = 'fixed';
-ele.style.left = (pos) + 'px';
-ele.style.top = (ypos) + 'px';
-ele.style.width = (size) + 'px';
-ele.style.height = (size) + 'px';
-ele.style.borderRadius = `100%`
-ele.style.backgroundColor = `hsl(0, 100%, 50%)`;
-ele.style.borderRadius = '50%';
-ele.style.transform = `skewY(${howMuch}deg)`; 
+function stylingTie({id, left, rotate}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 207,
+	width: 20,
+	height: 20,
+	borderRadius: '30%',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 0},
+	transform: `rotate(${rotate}deg)`
+}
+objProto(config);
+getId(id).style.borderTop = `2px solid black`;
+getId(id).style.borderLeft = `2px solid black`;
+
 }
 
-function addMusicNote(ele, pos, ypos) {
-ele.style.position = 'fixed';
-ele.style.left = (pos) + 'px';
-ele.style.top = (ypos) + 'px';
+function stylingTie1({id, left, rotate}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 210,
+	width: 13,
+	height: 13,
+	borderRadius: '100%',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 0},
+}
+objProto(config);
 
-//let letter = document.createElement('p');
-ele.innerText = '♪';
-ele.style.fontWeight = 'bold';
-ele.style.fontSize = (window.screen.width/30) + 'px'
-//ele.appendChild(letter);	
+}
+
+function stylingBlush({id, left}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: 160,
+	width: 20,
+	height: 10,
+	borderRadius: '50%',
+	zIndex: 1,
+	backgroundColor: {h: 305, s: 100, l: 70},
+}
+objProto(config);
+
+}
+
+function stylingHeart({id, left, top, size, skewness}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: top,
+	width: size,
+	height: size,
+	borderRadius: '50%',
+	zIndex: 1,
+	backgroundColor: {h: 0, s: 100, l: 50},
+	transform: `skewY(${skewness}deg)`
+}
+objProto(config);
+
+}
+
+function addMusicNote({id, left, top}) {
+const config = {
+	element: getId(id),
+	position: 'fixed',
+	left: left,
+	top: top,
+	width: '',
+	height: '',
+	borderRadius: '',
+	zIndex: 1,
+	backgroundColor: {h: '', s: '', l: ''},
+	transform: ''
+
+}
+objProto(config);
+
+getId(id).innerText = '♪';
+getId(id).style.fontWeight = 'bold';
+getId(id).style.fontSize = (window.screen.width/30) + 'px'
+
 }
 
 function appendObj(objec, func, pos, mo, deg) {
@@ -215,7 +319,7 @@ emotion3.style.display = 'none';
 emotion4.style.display = 'none';
 emotion5.style.display = 'none';
 
-addMusicNote(emotion6, 222, 100);
+addMusicNote({id: 'emotion6' , left: 222, top: 100});
 emotion6.style.display = 'block';
 setTimeout(() => {emotion6.style.top = 20 + 'px';
 	emotion6.style.left = 50 + 'px';
@@ -224,39 +328,72 @@ setTimeout(() => {emotion6.style.top = 20 + 'px';
 }
 
 function mousedownEvent(e) {
+let mouth = document.getElementById('mouth');
 let eye1 = document.getElementById('eye1');
 let eye2 = document.getElementById('eye2');
-eye1.style.borderRadius = '0%';
-eye1.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-eye1.style.borderBottom = 'none';
-eye1.style.borderLeft = 'none';	
-eye1.style.transform = 'rotate(45deg)';
-eye1.style.left = 120 + 'px';	
-eye2.style.borderRadius = '0%';
-eye2.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-eye2.style.borderTop = 'none';
-eye2.style.borderRight = 'none';	
-eye2.style.transform = 'rotate(45deg)';
-eye2.style.left = 175 + 'px';	
+
+addMusicNote({id: 'emotion6' , left: 222, top: 100});
+getId('emotion6').style.display = 'block';
+getId('emotion6').innerText = '?';
+
+//eye1.style.borderRadius = '0%';
+//eye1.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+//eye1.style.borderBottom = 'none';
+//eye1.style.borderLeft = 'none';	
+//eye1.style.transform = 'rotate(45deg)';
+//eye1.style.left = 120 + 'px';
+//eye2.style.borderRadius = '0%';
+//eye2.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+//eye2.style.borderTop = 'none';
+//eye2.style.borderRight = 'none';	
+//eye2.style.transform = 'rotate(45deg)';
+//eye2.style.left = 175 + 'px';
+//stylingSmile({id:'mouth4', left:140, top:180, width: 20, height: 13, skewness:10});
+//stylingSmile({id:'mouth5', left:145, top:180, width: 20, height: 13, skewness:-10});
+
+//getId('mouth4').style.display = 'block';
+//getId('mouth5').style.display = 'block';
+
 }
 
 function mouseupEvent(e) {
 let eye1 = document.getElementById('eye1');
 let eye2 = document.getElementById('eye2');
 let mouth = document.getElementById('mouth');
+let emotion = document.getElementById('emotion');
+let emotion1 = document.getElementById('emotion1');
 eye1.style.borderRadius = '0%';
 eye1.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-eye1.style.borderBottom = 'none';
+eye1.style.border = '3px solid black';	
+eye1.style.borderTop = 'none';
 eye1.style.borderLeft = 'none';	
-eye1.style.transform = 'rotate(45deg)';
-eye1.style.left = 120 + 'px';	
+eye1.style.borderRight = 'none';
+//eye1.style.transform = 'rotate(45deg)';
+eye1.style.left = 114 + 'px';	
 eye2.style.borderRadius = '0%';
 eye2.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+eye2.style.border = '3px solid black';
 eye2.style.borderTop = 'none';
 eye2.style.borderRight = 'none';	
-eye2.style.transform = 'rotate(45deg)';
-eye2.style.left = 175 + 'px';	
+eye2.style.borderLeft = 'none';
+//eye2.style.transform = 'rotate(45deg)';
+eye2.style.left = 172 + 'px';	
 mouth.style.backgroundColor = 'hsl(0, 100%, 50%)';
+stylingHeart({id: 'emotion7', left: 130, top: 190, size: 13, skewness: 0});
+stylingHeart({id: 'emotion8', left: 120, top: 193, size: 20, skewness: 0});	
+
+getId('emotion7').style.backgroundColor = 'hsl(60, 25%, 75%)';
+getId('emotion7').style.borderRadius = '0%';
+getId('emotion7').style.zIndex = 1;
+getId('emotion7').style.display = 'block';
+getId('emotion7').style.transform = 'rotate(45deg)';
+
+getId('emotion8').style.backgroundColor = 'hsl(60, 25%, 75%)';
+getId('emotion8').style.borderRadius = '100%';
+getId('emotion8').style.zIndex = 1;
+getId('emotion8').style.display = 'block';
+
+getId('emotion6').style.display = 'none';
 
 }
 
@@ -273,12 +410,12 @@ let emotion6 = document.getElementById('emotion6');
 
 emotion6.style.display = 'none';
 
-addHearts(emotion, 222, 25, 150, 13);
-addHearts(emotion1, 227, -25, 150, 13);
-addHearts(emotion2, 232, 25, 120, 20);
-addHearts(emotion3, 239, -25, 120, 20);
-addHearts(emotion4, 242, 25, 85, 26);
-addHearts(emotion5, 251, -25, 85, 26);
+stylingHeart({id: 'emotion', left: 222, top: 150, size: 13, skewness: 25});
+stylingHeart({id: 'emotion1', left: 227, top: 150, size: 13, skewness: -25});	
+stylingHeart({id: 'emotion2', left: 232, top: 120, size: 20, skewness: 25});	
+stylingHeart({id: 'emotion3', left: 239, top: 120, size: 20, skewness: -25});	
+stylingHeart({id: 'emotion4', left: 242, top: 85, size: 26, skewness: 25});	
+stylingHeart({id: 'emotion5', left: 251, top: 85, size: 26, skewness: -25});	
 
 if (scroll %  300 < 100) {
 emotion.style.display = 'block';
@@ -340,47 +477,31 @@ emotion3.style.display = 'none';
 emotion4.style.display = 'none';
 emotion5.style.display = 'none';
 emotion6.style.display = 'none';
+getId('emotion7').style.display = 'none';
+getId('emotion8').style.display = 'none';
 
-  y = 0;
-  x = 0;
-	  //y = e.clientY;
-//x = e.clientX;
-face.style.position = 'fixed';
-face.style.left = (x + 100) + 'px';
-face.style.top = (y + 100) + 'px';
-face.style.width = '100px';
-face.style.height = '100px';
-face.style.borderRadius = '30%';
-face.style.zIndex = '1';
-face.style.backgroundColor = `hsl(0, 100%, 85%)`;
-//face.style.border = `2px solid black`
-stylingEar(ear1, 100, 20);
-stylingEar(ear2, 160, -20);
-stylingEar2(ear3, 171, -20);
-stylingEar2(ear4, 111, 20);
-stylingEye(eye1, 120);
-stylingEye(eye2, 170);
-stylingMouth(mouth, 148, 45);
-stylingMouth1(mouth1, 153);
-stylingMouth2(mouth2, 148, 270);
-stylingMouth3(mouth3, 148, 0);
-stylingtie(tie, 129, 45);
-stylingtie(tie1, 159, 45);
-stylingtie3(tie2, 149, 0);
-stylingblush(tie3, 121, 0);
-stylingblush(tie4, 171, 0);
+stylingFace();
+stylingEar({id: 'ear1', left: 80, skewness: 20});
+stylingEar({id: 'ear2', left: 160, skewness: -20});
+stylingEarHole({id: 'ear3', left: 171, skewness: -20});
+stylingEarHole({id: 'ear4', left: 100, skewness: 20});
 
-	  /*
-	  addHearts(emotion, 222, 25, 150, 13);
-addHearts(emotion1, 227, -25, 150, 13);
-addHearts(emotion2, 232, 25, 120, 20);
-addHearts(emotion3, 239, -25, 120, 20);
-addHearts(emotion4, 242, 25, 85, 26);
-addHearts(emotion5, 251, -25, 85, 26);
-*/
+stylingEye({id: 'eye1', left: 114});
+stylingEye({id: 'eye2', left: 170});
+stylingMouth({id: 'mouth', left: 148, rotate: 45});
+stylingMouth1({id: 'mouth1', left: 152});
+stylingMouth2({id: 'mouth2', left: 147, rotate: 270});
+stylingMouth3({id: 'mouth3', left: 145, rotate: 0});
+stylingTie({id: 'tie', left: 129, rotate: 45});
+stylingTie({id: 'tie1', left: 159, rotate: 45});
+stylingTie1({id: 'tie2', left: 148});
+stylingBlush({id: 'blush', left: 113});
+stylingBlush({id: 'blush1', left: 171});
 
-eye1.style.left = (Math.sin(num/10) * 6 + 120) + 'px';
-eye2.style.left = (Math.sin(num/10) * 6 + 170) + 'px';
+eye1.style.left = (Math.sin(num/10) * 6 + 114) + 'px';
+eye2.style.left = (Math.sin(num/10) * 6 + 172) + 'px';
+getId('mouth').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+
   return;
   }
   let num = parseInt(localStorage.mouseCounter);
