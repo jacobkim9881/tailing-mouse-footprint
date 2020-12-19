c06881935c529d0fe8b8b10d83ae55a11fbb34ab
jacobkim9881 <jacobkim9881@gmail.com>
2020-12-19 21:45:51 +0900

    resfactor: bunny obj resfactoring

--- functions/bunny.js
+++ functions/bunny.js
@@ -77,7 +77,7 @@ const config = {
 	transform: 'none'	
 }
 objProto(config);
-getId('eye1').style.border = '2px solid black';
+getId(id).style.border = '2px solid black';
 
 }
 
@@ -298,87 +298,52 @@ getId(id).style.fontSize = (window.screen.width/30) + 'px'
 
 }
 
-function appendObj(objec, func, pos, mo, deg) {
-objec = document.createElement('div');
-func(objec, pos, deg);
-mo.appendChild(objec);
-}
-
-function dblclickEvent(e) {
-let emotion = document.getElementById('emotion');
-let emotion1 = document.getElementById('emotion1');
-let emotion2 = document.getElementById('emotion2');
-let emotion3 = document.getElementById('emotion3');
-let emotion4 = document.getElementById('emotion4');
-let emotion5 = document.getElementById('emotion5');
-let emotion6 = document.getElementById('emotion6');
-emotion.style.display = 'none';
-emotion1.style.display = 'none';
-emotion2.style.display = 'none';
-emotion3.style.display = 'none';
-emotion4.style.display = 'none';
-emotion5.style.display = 'none';
-
-addMusicNote({id: 'emotion6' , left: 222, top: 100});
-emotion6.style.display = 'block';
-setTimeout(() => {emotion6.style.top = 20 + 'px';
-	emotion6.style.left = 50 + 'px';
-        emotion6.innerText = '♩'}, 200);
-
-}
-
-function mousedownEvent(e) {
-let mouth = document.getElementById('mouth');
-let eye1 = document.getElementById('eye1');
-let eye2 = document.getElementById('eye2');
-
+function musicNoteAni() {
 addMusicNote({id: 'emotion6' , left: 222, top: 100});
 getId('emotion6').style.display = 'block';
-getId('emotion6').innerText = '?';
-
-//eye1.style.borderRadius = '0%';
-//eye1.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-//eye1.style.borderBottom = 'none';
-//eye1.style.borderLeft = 'none';	
-//eye1.style.transform = 'rotate(45deg)';
-//eye1.style.left = 120 + 'px';
-//eye2.style.borderRadius = '0%';
-//eye2.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-//eye2.style.borderTop = 'none';
-//eye2.style.borderRight = 'none';	
-//eye2.style.transform = 'rotate(45deg)';
-//eye2.style.left = 175 + 'px';
-//stylingSmile({id:'mouth4', left:140, top:180, width: 20, height: 13, skewness:10});
-//stylingSmile({id:'mouth5', left:145, top:180, width: 20, height: 13, skewness:-10});
-
-//getId('mouth4').style.display = 'block';
-//getId('mouth5').style.display = 'block';
+setTimeout(() => {getId('emotion6').style.top = 20 + 'px';
+	getId('emotion6').style.left = 50 + 'px';
+        getId('emotion6').innerText = '♩'}, 200);
 
 }
 
-function mouseupEvent(e) {
-let eye1 = document.getElementById('eye1');
-let eye2 = document.getElementById('eye2');
-let mouth = document.getElementById('mouth');
-let emotion = document.getElementById('emotion');
-let emotion1 = document.getElementById('emotion1');
-eye1.style.borderRadius = '0%';
-eye1.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-eye1.style.border = '3px solid black';	
-eye1.style.borderTop = 'none';
-eye1.style.borderLeft = 'none';	
-eye1.style.borderRight = 'none';
-//eye1.style.transform = 'rotate(45deg)';
-eye1.style.left = 114 + 'px';	
-eye2.style.borderRadius = '0%';
-eye2.style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
-eye2.style.border = '3px solid black';
-eye2.style.borderTop = 'none';
-eye2.style.borderRight = 'none';	
-eye2.style.borderLeft = 'none';
-//eye2.style.transform = 'rotate(45deg)';
-eye2.style.left = 172 + 'px';	
-mouth.style.backgroundColor = 'hsl(0, 100%, 50%)';
+function sleepyEyes() {
+getId('eye1').style.borderRadius = '0%';
+getId('eye1').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+getId('eye1').style.border = '3px solid black';	
+getId('eye1').style.borderTop = 'none';
+getId('eye1').style.borderLeft = 'none';	
+getId('eye1').style.borderRight = 'none';
+getId('eye1').style.left = 114 + 'px';	
+getId('eye2').style.borderRadius = '0%';
+getId('eye2').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+getId('eye2').style.border = '3px solid black';
+getId('eye2').style.borderTop = 'none';
+getId('eye2').style.borderRight = 'none';	
+getId('eye2').style.borderLeft = 'none';
+getId('eye2').style.left = 172 + 'px';	
+
+}
+
+function excitingEyes() {
+getId('eye1').style.borderRadius = '0%';
+getId('eye1').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+getId('eye1').style.border = '3px solid black';	
+getId('eye1').style.borderBottom = 'none';
+getId('eye1').style.borderLeft = 'none';	
+getId('eye1').style.transform = 'rotate(45deg)';
+getId('eye1').style.left = 120 + 'px';
+getId('eye2').style.borderRadius = '0%';
+getId('eye2').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+getId('eye2').style.border = '3px solid black';	
+getId('eye2').style.borderTop = 'none';
+getId('eye2').style.borderRight = 'none';	
+getId('eye2').style.transform = 'rotate(45deg)';
+getId('eye2').style.left = 175 + 'px';
+
+}
+
+function sighEmoj() {
 stylingHeart({id: 'emotion7', left: 130, top: 190, size: 13, skewness: 0});
 stylingHeart({id: 'emotion8', left: 120, top: 193, size: 20, skewness: 0});	
 
@@ -397,18 +362,100 @@ getId('emotion6').style.display = 'none';
 
 }
 
+function questionEmoj() {
+addMusicNote({id: 'emotion6' , left: 222, top: 100});
+getId('emotion6').style.display = 'block';
+getId('emotion6').innerText = '?';
+
+}
+
+function heartEmoj(num) {
+ switch(num) {
+  case 1:
+  getId('emotion').style.display = 'block';
+getId('emotion1').style.display = 'block';
+getId('emotion2').style.display = 'none';
+getId('emotion3').style.display = 'none';
+getId('emotion4').style.display = 'none';
+getId('emotion5').style.display = 'none';
+  break;
+  case 2:
+getId('emotion').style.display = 'block';
+getId('emotion1').style.display = 'block';
+getId('emotion2').style.display = 'block';
+getId('emotion3').style.display = 'block';
+getId('emotion4').style.display = 'none';
+getId('emotion5').style.display = 'none';
+break;
+  case 3:
+getId('emotion').style.display = 'block';
+getId('emotion1').style.display = 'block';
+getId('emotion2').style.display = 'block';
+getId('emotion3').style.display = 'block';
+getId('emotion4').style.display = 'block';
+getId('emotion5').style.display = 'block';
+break;
+  default:
+getId('emotion').style.display = 'none';
+getId('emotion1').style.display = 'none';
+getId('emotion2').style.display = 'none';
+getId('emotion3').style.display = 'none';
+getId('emotion4').style.display = 'none';
+getId('emotion5').style.display = 'none';
+break;
+
+ }
+}
+
+function dragEndEvent(e) {
+getId('mouth').style.backgroundColor = 'hsl(0, 100%, 50%)';
+
+}
+
+function dragEvent(e) {
+stylingEye({id: 'eye1', left: 114});
+stylingEye({id: 'eye2', left: 170});
+getId('mouth').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+questionEmoj();
+}
+
+function dblclickEvent(e) {
+let none = null;	
+heartEmoj(none);
+
+excitingEyes();
+
+getId('mouth').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
+
+getId('emotion7').style.display = 'none';
+getId('emotion8').style.display = 'none';
+
+musicNoteAni();
+
+}
+
+function mousedownEvent(e) {
+questionEmoj();
+
+sleepyEyes();
+
+getId('eye1').style.transform = 'rotate(0deg)';
+getId('eye2').style.transform = 'rotate(0deg)';
+
+}
+
+function mouseupEvent(e) {
+sleepyEyes();
+
+getId('mouth').style.backgroundColor = 'hsl(0, 100%, 50%)';
+
+sighEmoj();
+}
+
 function onScrollEvent(e) {
-let scroll = window.scrollY;
+let typeOfEmoj = Math.ceil((window.scrollY% 300)/100);
 
-let emotion = document.getElementById('emotion');
-let emotion1 = document.getElementById('emotion1');
-let emotion2 = document.getElementById('emotion2');
-let emotion3 = document.getElementById('emotion3');
-let emotion4 = document.getElementById('emotion4');
-let emotion5 = document.getElementById('emotion5');
-let emotion6 = document.getElementById('emotion6');
-
-emotion6.style.display = 'none';
+getId('emotion6').style.display = 'none';
 
 stylingHeart({id: 'emotion', left: 222, top: 150, size: 13, skewness: 25});
 stylingHeart({id: 'emotion1', left: 227, top: 150, size: 13, skewness: -25});	
@@ -417,66 +464,15 @@ stylingHeart({id: 'emotion3', left: 239, top: 120, size: 20, skewness: -25});
 stylingHeart({id: 'emotion4', left: 242, top: 85, size: 26, skewness: 25});	
 stylingHeart({id: 'emotion5', left: 251, top: 85, size: 26, skewness: -25});	
 
-if (scroll %  300 < 100) {
-emotion.style.display = 'block';
-emotion1.style.display = 'block';
-emotion2.style.display = 'none';
-emotion3.style.display = 'none';
-emotion4.style.display = 'none';
-emotion5.style.display = 'none';
-
-} else if (scroll % 300 < 200) {
-emotion.style.display = 'block';
-emotion1.style.display = 'block';
-emotion2.style.display = 'block';
-emotion3.style.display = 'block';
-emotion4.style.display = 'none';
-emotion5.style.display = 'none';
-} else if (scroll % 300 < 300) {
-emotion.style.display = 'block';
-emotion1.style.display = 'block';
-emotion2.style.display = 'block';
-emotion3.style.display = 'block';
-emotion4.style.display = 'block';
-emotion5.style.display = 'block';
-}
+heartEmoj(typeOfEmoj);
 }
 
 function mouseEvent(e) {
-  let grpDivs = document.getElementById('grpDivs');
 
   function trigger(e, moveX) {
-   let face = document.getElementById('face');
-let eye1 = document.getElementById('eye1');
-let eye2 = document.getElementById('eye2');
-let ear1 = document.getElementById('ear1');
-let ear2 = document.getElementById('ear2');
-let ear3 = document.getElementById('ear3');
-let ear4 = document.getElementById('ear4');
-let mouth = document.getElementById('mouth');
-let mouth1 = document.getElementById('mouth1');
-let mouth2 = document.getElementById('mouth2');
-let mouth3 = document.getElementById('mouth3');
-let tie = document.getElementById('tie');
-let tie1 = document.getElementById('tie1');
-let tie2 = document.getElementById('tie2');
-let tie3 = document.getElementById('tie3');
-let tie4 = document.getElementById('tie4');
+let none = null;
 
-let emotion = document.getElementById('emotion');
-let emotion1 = document.getElementById('emotion1');
-let emotion2 = document.getElementById('emotion2');
-let emotion3 = document.getElementById('emotion3');
-let emotion4 = document.getElementById('emotion4');
-let emotion5 = document.getElementById('emotion5');
-let emotion6 = document.getElementById('emotion6');
-emotion.style.display = 'none';
-emotion1.style.display = 'none';
-emotion2.style.display = 'none';
-emotion3.style.display = 'none';
-emotion4.style.display = 'none';
-emotion5.style.display = 'none';
-emotion6.style.display = 'none';
+heartEmoj(none);
 getId('emotion7').style.display = 'none';
 getId('emotion8').style.display = 'none';
 
@@ -485,7 +481,6 @@ stylingEar({id: 'ear1', left: 80, skewness: 20});
 stylingEar({id: 'ear2', left: 160, skewness: -20});
 stylingEarHole({id: 'ear3', left: 171, skewness: -20});
 stylingEarHole({id: 'ear4', left: 100, skewness: 20});
-
 stylingEye({id: 'eye1', left: 114});
 stylingEye({id: 'eye2', left: 170});
 stylingMouth({id: 'mouth', left: 148, rotate: 45});
@@ -498,8 +493,8 @@ stylingTie1({id: 'tie2', left: 148});
 stylingBlush({id: 'blush', left: 113});
 stylingBlush({id: 'blush1', left: 171});
 
-eye1.style.left = (Math.sin(num/10) * 6 + 114) + 'px';
-eye2.style.left = (Math.sin(num/10) * 6 + 172) + 'px';
+getId('eye1').style.left = (Math.sin(num/10) * 6 + 114) + 'px';
+getId('eye2').style.left = (Math.sin(num/10) * 6 + 172) + 'px';
 getId('mouth').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
 
   return;
@@ -507,10 +502,10 @@ getId('mouth').style.backgroundColor = 'hsl(0, 0%, 0%, 0)';
   let num = parseInt(localStorage.mouseCounter);
   localStorage.mouseCounter = num + 1;
    trigger(e, 0);  
-  grpDivs.style.transform = 'scale(0.3, 0.3)'
-  grpDivs.style.position = 'fixed';
-  grpDivs.style.top = e.clientY + 'px';
-  grpDivs.style.left = e.clientX + 'px';
+  getId('grpDivs').style.transform = 'scale(0.3, 0.3)'
+  getId('grpDivs').style.position = 'fixed';
+  getId('grpDivs').style.top = e.clientY + 'px';
+  getId('grpDivs').style.left = e.clientX + 'px';
 }
 
 //createObj
@@ -520,6 +515,9 @@ document.body.addEventListener('mousemove', mouseEvent);
 document.body.addEventListener('mousedown', mousedownEvent);
 document.body.addEventListener('mouseup', mouseupEvent);
 document.body.addEventListener('dblclick', dblclickEvent);
+document.body.addEventListener('drag', dragEvent);
+document.body.addEventListener('dragend', dragEndEvent);
+
 window.addEventListener('scroll', onScrollEvent);
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.name === 'bunny') {
@@ -531,6 +529,8 @@ chrome.runtime.onMessage.addListener((msg) => {
     document.body.removeEventListener('mouseup', mouseupEvent);
     document.body.removeEventListener('scroll', onScrollEvent);
     document.body.removeEventListener('dblclick', dblclickEvent);
+    document.body.removeEventListener('drag', dragEvent);
+    document.body.removeEventListener('dragend', dragEvent);
 
   }
 });
