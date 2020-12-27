ddea101b24019e77e54e9c1d4b3fd0c77b1ee243
jacobkim9881 <jacobkim9881@gmail.com>
2020-12-27 23:45:50 +0900

    feature: added onclick event

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,5 @@
 //bunny.js
-localStorage.mouseObjArr = ['face',"leaf0", "leaf1", "leaf2", "leaf3", "leaf4", "leaf5", "leaf6", "leaf7", "leaf8", "leaf9", "leaf10", "leaf11", "leaf12", "leaf13", "leaf14", "leaf15", "leaf16", "leaf17", "leaf18", "leaf19", "leaf20", "leaf21", "leaf22", "leaf23", "leaf24", "leaf25", "leaf26", "leaf27", "leaf28", "leaf29", "leaf30", "leaf31", "leaf32", "leaf33", "leaf34", "leaf35"];
+localStorage.mouseObjArr = ['face', "green0", "leaf0", "leaf1", "leaf2", "leaf3", "leaf4", "leaf5", "leaf6", "leaf7", "leaf8", "leaf9", "leaf10", "leaf11", "leaf12", "leaf13", "leaf14", "leaf15", "leaf16", "leaf17", "leaf18", "leaf19", "leaf20", "leaf21", "leaf22", "leaf23", "leaf24", "leaf25", "leaf26", "leaf27", "leaf28", "leaf29", "leaf30", "leaf31", "leaf32", "leaf33", "leaf34", "leaf35"];
 
 function createObjs() {
   let grpDivs = document.createElement('div');
@@ -38,7 +38,7 @@ function getId(id) {
   return document.getElementById(id);
 }
 
-function stylingFace() {
+function stylingFace({h, s, l, h1, s1, l1}) {
   const config = {
     element: getId('face'),
     position: 'fixed',
@@ -53,11 +53,11 @@ function stylingFace() {
 
   }
   objProto(config);
-  getId('face').style.backgroundImage = 'radial-gradient(hsl(22, 100%, 16%), hsl(22, 100%, 31%))';
+  getId('face').style.backgroundImage = `radial-gradient(hsl(${h}, ${s}%, ${l}%), hsl(${h1}, ${s1}%, ${l1}%))`;
 
 }
 
-function stylingEar({id, top, left, skewness}) {
+function stylingLeaf({id, top, left, skewness, h, s, l, h1, s1, l1}) {
   const config = {
     element: getId(id),
     position: 'fixed',
@@ -71,51 +71,125 @@ function stylingEar({id, top, left, skewness}) {
     transform: `rotate(${skewness}deg)`
   }
   objProto(config);
+  getId(id).className = 'leafs';	
   getId(id).style.borderRadius = '100% 100% 50% 50%';
-  getId(id).style.backgroundImage = 'linear-gradient(hsl(232, 100%, 50%), hsl(63, 100%, 50%)';
+  getId(id).style.backgroundImage = `linear-gradient(hsl(${h}, ${s}%, ${l}%), hsl(${h1}, ${s1}%, ${l1}%)`;
 
 }
+
+function stylingGreen({id}) {
+  const config = {
+    element: getId(id),
+    position: 'fixed',
+    left: 50,
+    top: 230,
+    width: 80,
+    height: 330,
+    borderRadius: '50%',
+    zIndex: 0,
+    backgroundColor: {h: 0, s:0, l:0},
+    transform: `rotate(${0}deg)`
+  }
+  objProto(config);
+  getId(id).style.borderLeft = '20px solid green';
+  getId(id).style.backgroundColor = 'hsl(0, 100%, 50%, 0)';
+}
+
+function onclickEvent(e) {
+  /* 
+   name: 'Aster',
+  	    face: {h: ,s: , l: ,h1: , s1: ,l1: },
+	    leaf: {h: ,s: , l: ,h1: , s1: ,l1: }
+  */
+  let flowers = [
+    {
+	    name: 'Aster',
+  	    face: "radial-gradient(hsl(63, 100%, 50%), hsl(45, 100%, 50%))",
+	    leaf: "linear-gradient(hsl(300, 100%, 90%), hsl(300, 100%, 77%))"
+
+    },
+    {
+	    name: 'Black-eye-susan',
+  	    face: "radial-gradient(hsl(29, 100%, 10%), hsl(29, 100%, 15%))",
+	    leaf: "linear-gradient(hsl(55, 100%, 50%), hsl(55, 100%, 40%))"
+
+    },
+    {
+	    name: 'orange-pink',
+  	    face: "radial-gradient(hsl(29, 100%, 10%), hsl(29, 33%, 48%))",
+	    leaf: "linear-gradient(hsl(20, 100%, 65%), hsl(300, 100%, 85%))"
+
+    }
+
+
+  ]
+  let randomNum = Math.trunc(Math.random() * (flowers.length));
+	console.log(randomNum)
+  getId('face').style.backgroundImage = flowers[randomNum].face;
+  function resolveColor(id, time) {
+    new Promise(() => {
+      setTimeout(() => {
+      id.style.backgroundImage = flowers[randomNum].leaf;
+      },15 * parseInt(time, 10))	    
+    })	 
+  }
+  async function asyncCall() {       
+    let leafs = document.querySelectorAll('.leafs') 	 
+    for (let leaf in leafs) {
+     if (leafs[leaf].className){ 	    
+     await resolveColor(leafs[leaf], leaf);
+     }
+    }
+  }
+
+  asyncCall();	
+}
+
 function mouseEvent(e) {
 
   function trigger(e, moveX) {
     let none = null;
-    stylingFace();
-  
-    stylingEar({id: `leaf0`, top: 0, left: 114, skewness:-0});
-    stylingEar({id: `leaf1`, top: 170, left: 114, skewness:180});
-    stylingEar({id: `leaf2`, top: 85, left: 200, skewness:90});	  
-    stylingEar({id: `leaf3`, top: 85, left: 40, skewness:-90});
+    stylingFace({h:22 ,s:100 , l:16 ,h1:22 , s1:100 ,l1:31 });
+//232, 100%, 50%), hsl(63, 100%, 50%)
 
-    stylingEar({id: `leaf4`, top: 25, left: 174, skewness:45});
-    stylingEar({id: `leaf5`, top: 135, left: 174, skewness:135});
-    stylingEar({id: `leaf6`, top:135,  left: 54, skewness:-135});
-    stylingEar({id: `leaf7`, top: 25,left: 54, skewness:-45});
+    stylingLeaf({id: `leaf0`, top: 0, left: 114, skewness:-0, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf1`, top: 170, left: 114, skewness:180, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf2`, top: 85, left: 200, skewness:90, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });	  
+    stylingLeaf({id: `leaf3`, top: 85, left: 40, skewness:-90, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+
+    stylingLeaf({id: `leaf4`, top: 25, left: 174, skewness:45, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf5`, top: 135, left: 174, skewness:135, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf6`, top:135,  left: 54, skewness:-135, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf7`, top: 25,left: 54, skewness:-45, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
 	  
-    stylingEar({id: `leaf8`, top: 12, left: 134, skewness:22});
-    stylingEar({id: `leaf9`, top: 147, left: 134, skewness:157});
-    stylingEar({id: `leaf10`,top: 147, left: 74, skewness:-157});
-    stylingEar({id: `leaf11`,top: 12, left: 74, skewness:-22});
+    stylingLeaf({id: `leaf8`, top: 12, left: 134, skewness:22, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf9`, top: 147, left: 134, skewness:157, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf10`,top: 147, left: 74, skewness:-157, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf11`,top: 12, left: 74, skewness:-22, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
 
-    stylingEar({id: `leaf12`, top: 55, left: 186, skewness:67});
-    stylingEar({id: `leaf13`, top: 110, left: 186, skewness:112});
-    stylingEar({id: `leaf14`,top: 110, left: 47, skewness:-112});
-    stylingEar({id: `leaf15`,top: 55, left: 47, skewness:-67});
+    stylingLeaf({id: `leaf12`, top: 55, left: 186, skewness:67, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf13`, top: 110, left: 186, skewness:112, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf14`,top: 110, left: 47, skewness:-112, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
+    stylingLeaf({id: `leaf15`,top: 55, left: 47, skewness:-67, h:232 ,s:100 , l:50 ,h1:63 , s1:100 ,l1:50 });
 
+    stylingGreen({id: "green0"});	  
       return;
   }
   let num = parseInt(localStorage.mouseCounter);
   localStorage.mouseCounter = num + 1;
   trigger(e, 0);  
-  getId('grpDivs').style.transform = `scale(0.2, 0.2)`;
+  getId('grpDivs').style.transform = `scale(0.1, 0.1)`;
   getId('grpDivs').style.position = 'fixed';
   getId('grpDivs').style.top = e.clientY + 'px';
-  getId('grpDivs').style.left = e.clientX + 'px';
+  getId('grpDivs').style.left = (e.clientX + 10 )+ 'px';
 }
 
 //createObj
 createObjs();
 
 document.body.addEventListener('mousemove', mouseEvent);
+document.body.addEventListener('click', onclickEvent);
+
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.name === 'bunny') {
     document.getElementById('grpDivs').style.display = 'block'; 
