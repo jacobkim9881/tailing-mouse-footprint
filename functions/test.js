6aabf232f1ba7ff4e6db431e1cff1ed4c3d55b83
jacobkim9881 <jacobkim9881@gmail.com>
2021-02-10 23:20:50 +0900

    feat: made balloons

--- functions/test.js
+++ functions/test.js
@@ -4,12 +4,13 @@ function mouseEvent(e) {
 //let ranH = num %  360;
 
   function trigger(e) {
-  let obj = document.createElement('div');
+  let obj = document.createElement('div');  	  
   let ballSize = window.innerWidth/100;
   let randomBallSize = Math.trunc(Math.random() * ballSize);
   let ranXpos = parseInt(e.clientX, 10);
   let ranYpos = parseInt(e.clientY, 10);
 //  let ranH = Math.trunc(Math.random() * 360);
+  obj.className = 'flubbers'	  
   obj.style.position = 'fixed';
   obj.style.left = ranXpos + 'px';
   obj.style.width = ballSize + 'px';
@@ -20,47 +21,58 @@ obj.style.top= ranYpos + 'px';
 
 
   obj.animate([
-    {top: (ranYpos * 1.05) + 'px',
+    {top: (ranYpos * 0.96) + 'px',
 	    transform: 'scale(1,1)',
 	    width: (ballSize * 1.1) + 'px',
 	    borderRadius: '70% 70% 50% 50%',
+	    opacity: '1',
      backgroundColor: `hsl(${ranH}, 100%, 50%)`},
-    {top: ranYpos + 'px',
+    {top: (ranYpos * 0.8 )+ 'px',
     transform: 'scale(0.95, 0.95)',
 	    width: (ballSize * 0.95) + 'px',
 	    borderRadius: '50% 50% 50% 50%',
+	    opacity: '1',
 
     backgroundColor: `hsl(${ranH + 10}, 100%, 50%)`},
-    {top: (ranYpos * 1.03) + 'px',
+    {top: (ranYpos * 0.6) + 'px',
     transform: 'scale(1,1)',
 	    width: (ballSize * 1.05) + 'px',
  borderRadius: '60% 60% 50% 50%',
+	    opacity: '0.8',
 
     backgroundColor: `hsl(${ranH + 20}, 100%, 50%)`},
-    {top: ranYpos + 'px',
+    {top: (ranYpos * 0.3) + 'px',
     transform: 'scale(0.95, 0.95)',
     width: (ballSize * 1) + 'px',
  borderRadius: '50% 50% 50% 50%',
+	    opacity: '0.3',
 
     backgroundColor: `hsl(${ranH + 30}, 100%, 50%)`},
-  ], 500)
+  ], 5000)
   
   document.body.appendChild(obj);
-  setTimeout(() => obj.remove(), 400);
+  setTimeout(() => obj.remove(), 4900);
   return;
   }
   let num = parseInt(localStorage.mouseCounter);
   localStorage.mouseCounter = num + 1;
   let ranH = num %  360;
 
-  if (num %  3 === 0 ) {
+  if (num %  7 === 0 ) {
    trigger(e);
 
   }
 }
 
+function clickEvent() {
+let objs = document.getElementsByClassName('flubbers');
+console.log(objs[0].backgroundColor)
+}
+
 document.body.addEventListener('mousemove', mouseEvent);
+document.body.addEventListener('click', clickEvent);
 
 chrome.runtime.onMessage.addListener((msg) => {
-  document.body.removeEventListener('mousemove', mouseEvent)
+  document.body.removeEventListener('mousemove', mouseEvent);
+  document.body.removeEventListener('click', clickEvent);
 });
