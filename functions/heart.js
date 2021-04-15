e48986e930e64f5db46459aeb11258ac333d4997
jacobkim9881 <jacobkim9881@gmail.com>
2021-04-15 18:30:25 +0900

    feat: changed heart size and shape

--- functions/heart.js
+++ functions/heart.js
@@ -5,9 +5,12 @@ function mouseEvent(e) {
   function trigger(e, moveX) {
     let mouseMoveCounter = localStorage.mouseCounter;
     let halfHeart = document.createElement('div');
+    let ran7 = Math.trunc(Math.random() * 7);  
+    let colorArr = ['#ff0000', '#ff7700', '#fbff00', '#08ff00', '#0011ff', '#5900ff', '#ff00e1'];
+    let randomColor = colorArr[ran7];
     let ranXpos = Math.trunc(Math.random() * 15) + parseInt(e.clientX, 10);
-    let ranYpos = Math.trunc(Math.random() * 10) + parseInt(e.clientY, 10);
-    let ran10 = Math.trunc(Math.random() * 5) + window.innerWidth/100;
+    let ranYpos = Math.trunc(Math.random() * 20) + parseInt(e.clientY, 10);
+    let ran10 = Math.trunc(Math.random() * 8) + window.innerWidth/200;
     let ranYpos2 = ranYpos + ran10;
     let secLeft = ranXpos + ran10/2; 
     halfHeart.style.position = 'fixed';
@@ -18,7 +21,7 @@ function mouseEvent(e) {
     }  
     halfHeart.style.width = ran10 + 'px';
     halfHeart.style.height = ran10 + 'px';
-    halfHeart.style.backgroundColor = `hsl(0, 100%, 50%)`;
+    halfHeart.style.backgroundColor = randomColor;	  
     halfHeart.style.borderRadius = '50%';  
     halfHeart.style.transform = 'skewY(25deg)';
 
@@ -30,14 +33,14 @@ function mouseEvent(e) {
     ], 500);
   
     document.body.appendChild(halfHeart);
-    setTimeout(() => halfHeart.remove(), 500);
+    setTimeout(() => halfHeart.remove(), 490);
   
     let OtherHalfHeart = document.createElement('div');
     OtherHalfHeart.style.position = 'fixed';
     OtherHalfHeart.style.left = secLeft + 'px';
     OtherHalfHeart.style.width = ran10 + 'px';
     OtherHalfHeart.style.height = ran10 + 'px';
-    OtherHalfHeart.style.backgroundColor = `hsl(0, 100%, 50%)`;
+    OtherHalfHeart.style.backgroundColor = randomColor;
     OtherHalfHeart.style.borderRadius = '50%';  
     OtherHalfHeart.style.transform = 'skewY(25deg)';
 
@@ -50,13 +53,11 @@ function mouseEvent(e) {
     ], 500);
   
     document.body.appendChild(OtherHalfHeart);
-    setTimeout(() => OtherHalfHeart.remove(), 500);
+    setTimeout(() => OtherHalfHeart.remove(), 490);
  
     return;
   }
-  let num = parseInt(localStorage.mouseCounter);
-  localStorage.mouseCounter = num + 1;
-  if (num %  5 === 0 ) {
+  if (new Date().getTime() %  13 === 0 ) {
     trigger(e, 0);
   }
 }
