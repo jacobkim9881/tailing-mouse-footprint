08e7cd5e161c617483ab1fd1c04e7c5a7846f2c9
jacobkim9881 <jacobkim9881@gmail.com>
2021-04-27 10:36:18 +0900

    test: color buble

--- functions/test.js
+++ functions/test.js
@@ -1,21 +1,27 @@
 //test.js
+function ranColor() {
+let ran360 = Math.trunc(Math.random() * 361);
+let ran100 = Math.trunc(Math.random() * 90) + 10;
+let ran100l = Math.trunc(Math.random() * 0) + 50;
+return `hsl(${ran360}, ${ran100}%, ${ran100l}%)`; 
+}
+    
 function mouseEvent(e) {
-// object for styling
   let obj = document.createElement('div');
-  
-	
+    
   let ran10 = Math.trunc(Math.random() * 10) + 5;
     let ranXpos = Math.trunc(Math.random() * ran10) + parseInt(e.clientX, 10);
     let ranYpos = Math.trunc(Math.random() * 10) + parseInt(e.clientY, 10);
     let ran20 = ranYpos - 30;
-    obj.style.position = 'fixed';
-    obj.style.backgroundImage = `linear-gradient(to bottom right, hsl(170, 100%, 50%), hsl(170, 100%, 0%)`;
+    let ran180 =  Math.trunc(Math.random() * 30) + 150; 
+    obj.style.position = 'fixed';   
+    obj.style.backgroundImage = `linear-gradient(${ran180}deg, ${ranColor()}, ${ranColor()}`;
     obj.style.borderRadius = '50%';  
-    obj.animate([
-      {top: ranYpos + 'px',
-      left: e.clientX + 'px',
-      width: 5 + 'px',
-      height: 5 + 'px' 
+    obj.animate([              
+      {top: ranYpos + 'px',    
+      left: e.clientX + 'px',  
+      width: 5 + 'px',         
+      height: 5 + 'px'         
     }, 
       {top: ran20 + 'px',
       left: ranXpos + 'px',
