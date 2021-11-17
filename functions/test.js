511fdb3b883afd8e394bfeb864c8bc89d175665c
jacobkim9881 <jacobkim9881@gmail.com>
2021-11-17 21:36:14 +0900

    test: test for physics

--- functions/test.js
+++ functions/test.js
@@ -1,36 +1,52 @@
 //test.js
 function mouseEvent(e) {
-// object for styling
-  let obj = document.createElement('div');
-  
-	
-  let ran10 = Math.trunc(Math.random() * 10) + 5;
-    let ranXpos = Math.trunc(Math.random() * ran10) + parseInt(e.clientX, 10);
-    let ranYpos = Math.trunc(Math.random() * 10) + parseInt(e.clientY, 10);
-    let ran20 = ranYpos - 30;
-    obj.style.position = 'fixed';
-    obj.style.backgroundImage = `linear-gradient(to bottom right, hsl(170, 100%, 50%), hsl(170, 100%, 0%)`;
-    obj.style.borderRadius = '50%';  
-    obj.animate([
-      {top: ranYpos + 'px',
-      left: e.clientX + 'px',
-      width: 5 + 'px',
-      height: 5 + 'px' 
-    }, 
-      {top: ran20 + 'px',
-      left: ranXpos + 'px',
-      width: ran10 + 'px',
-      height: ran10 + 'px' 
-    }
-    ], 200)
 
+  function trigger(e) {
+  let obj = document.createElement('div');
+  let ballSize = window.innerWidth/100;
+  let randomBallSize = Math.trunc(Math.random() * ballSize) + 5;
+  let ranXpos = ballSize + parseInt(e.clientX, 10);
+  let ranYpos = ballSize + parseInt(e.clientY, 10);
+  let ranH = Math.trunc(Math.random() * 360);
+  let ranH2 =  Math.trunc(Math.random() * 360);
+  let ranHeit = 80 + randomBallSize;	  
+  obj.style.position = 'fixed';
+  obj.style.left = ranXpos + 'px';
+  obj.style.width = randomBallSize + 'px';
+  obj.style.height = randomBallSize + 'px';
+  obj.style.backgroundColor = `hsl(${ranH}, 100%, 50%)`;
+  obj.style.borderRadius = '50%';
+  let aniArr = [
+    {top: (ranYpos) + 'px'},
+    {top: (ranYpos + ranHeit)+ 'px'},
+    {top: (ranYpos + ranHeit/2) + 'px'},
+    {top: (ranYpos + ranHeit) + 'px'},
+    {top: (ranYpos + ranHeit * 3/4) + 'px'},
+    {top: (ranYpos + ranHeit) + 'px'},
+    {top: (ranYpos + ranHeit * 7/8) + 'px'},
+    {top: (ranYpos + ranHeit) + 'px'},
+    {top: (ranYpos + ranHeit * 15/16) + 'px'},
+{top: (ranYpos + ranHeit) + 'px'}
+
+  ]
+  obj.animate( aniArr , {duration: 3000,
+      timing(timeFraction) {
+      return timeFraction}
+  })
+  
   document.body.appendChild(obj);
-  setTimeout(() => obj.remove(), 200);
+  setTimeout(() => obj.remove(), 3000);
   return;
+  }
+  let num = parseInt(localStorage.mouseCounter);
+  localStorage.mouseCounter = num + 1;
+  if (num %  4 === 0 ) {
+   trigger(e);
+  }
 }
 
 document.body.addEventListener('mousemove', mouseEvent);
 
 chrome.runtime.onMessage.addListener((msg) => {
-document.body.removeEventListener('mousemove', mouseEvent)
+  document.body.removeEventListener('mousemove', mouseEvent)
 });
