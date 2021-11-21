1646ea7ee84a284e4b000c3b2f6b5fcd93b02327
jacobkim9881 <jacobkim9881@gmail.com>
2021-11-21 10:37:35 +0900

    chore: cleaned unneeded code

--- functions/test.js
+++ functions/test.js
@@ -1,26 +1,31 @@
 //test.js
 
 function animateBar(obj, cls) {
-  let d = cls === 'off' ? 0 : 45;
-  let d2 = cls === 'on' ? 0 : 45;
-
-  obj.className = obj.className === 'off' ? 'on' : 'off';	
-
+  let d = cls === 'off' ? 0 : 45,
+  d2 = cls === 'on' ? 0 : 45,
+  c = cls === 'off' ? 'hsl(0, 100%, 100%, 0)' : 'hsl(0, 100%, 100%, 1)',
+  c2 = cls === 'on' ? 'hsl(0, 100%, 100%, 0)' : 'hsl(0, 100%, 100%, 1)';	
+  obj.className = obj.className === 'off' ? 'on' : 'off';
   obj.animate([
-      {transform: `rotate(${d}deg)`}, 
-      {transform: `rotate(${d2}deg)`}, 
+      {
+    transform: `rotate(${d}deg)`,
+    backgroundColor: c 	      
+      }, 
+      {
+    transform: `rotate(${d2}deg)`,
+    backgroundColor: c2 	      
+      }, 
     ], 200)
+  obj.style.backgroundColor = c2;
   obj.style.transform = `rotate(${d2}deg)`;
 }
 
 function mouseEvent(e) {
-
-  if (e.clientX % 13 < 5) {return}	
-  let num = Math.trunc(e.clientX / 45);
-  let preNum = localStorage.getItem('tmf-cnt') ? localStorage.getItem('tmf-cnt') : num;	
-  let obj = document.getElementById(`objs-test${num}`)
-  let cntWidth = Math.trunc(window.innerWidth / 45);
-  if (localStorage.getItem('tmf-cnt') === num.toString()) {return};	
+  let num = Math.trunc(e.clientX / 45),
+  preNum = localStorage.getItem('tmf-cnt') ? localStorage.getItem('tmf-cnt') : num,	
+  obj = document.getElementById(`objs-test${num}`),
+  cntWidth = Math.trunc(window.innerWidth / 45);
+  if (preNum === num.toString()) {return};	
   localStorage.setItem('tmf-cnt', num);	
   
   for (let i = 0; i < num; i++){
@@ -36,37 +41,35 @@ function mouseEvent(e) {
 }
 
 function setObjs() {
-//  if (document.getElementsByClassName('tail-stamp')) {return};	
-
   let oClass = {
    class: 'objs-test',
    width: '15px',
    height: '45px',
-   backgroundColor : 'white',
+   border: '1px solid white',	  
+   backgroundColor : 'none',
    top: (window.innerHeight -100) + 'px'
-  }
-  let cntWidth = Math.trunc(window.innerWidth / 45);
+  },
+  cntWidth = Math.trunc(window.innerWidth / 45);
   for (let i = 0; i < cntWidth; i++) {
     let oneObj = document.createElement('div');
     oneObj.id = oClass.class + i;
     oneObj.className = 'off';	  
     oneObj.style.width = oClass.width;
     oneObj.style.height = oClass.height;
-    oneObj.style.backgroundColor = oClass.backgroundColor;
+//    oneObj.style.backgroundColor = oClass.backgroundColor;
     oneObj.style.top = oClass.top;
     oneObj.style.left = 50 + i * 45 + 'px';
     oneObj.style.position = 'fixed';
+    oneObj.style.border = oClass.border;	
     document.body.appendChild(oneObj);	  
   }
-   let stamp = document.createElement('div');
-   stamp.className = 'tail-stamp';
-   document.body.appendChild(stamp);	  
- 
+
 }
 
 function deleteObjs() {
   let cntWidth = Math.trunc(window.innerWidth / 45);
   for (let i = 0; i < cntWidth; i++) {
+  if (!document.getElementById(`objs-test${i}`)) {continue;}
   document.getElementById(`objs-test${i}`).remove();
   }
 }
