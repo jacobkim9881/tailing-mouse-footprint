cc3771963e10a376471c5328bdbf60fce8b7cf6e
jacobkim9881 <jacobkim9881@gmail.com>
2021-12-14 16:22:22 +0900

    test: objectized functions

--- contents.js
+++ contents.js
@@ -3,15 +3,35 @@ chrome.runtime.sendMessage(undefined,
   { type : 'check',
     sender: 'contents'}
 );
-    
+console.log('contents')    
+
+function executeEvents(obj, name) {
+localStorage.pointerName = name;	
+ for (const [key, func] of Object.entries(obj)) {
+    document.body.addEventListener(key, func);
+console.log(func)
+	 console.log(name)
+	 console.log(key)
+    chrome.runtime.onMessage.addListener((msg) => {
+	    console.log(msg)
+     msg.name !== name ? document.body.removeEventListener(key, func) : null;
+    });
+ }
+
+}
+
+executeEvents(functionObj[localStorage.pointerName], localStorage.pointerName);
 
 chrome.runtime.onMessage.addListener((msg, _, sendRes) => {
-        
-  chrome.runtime.sendMessage(undefined,
-    { name:msg.name,
-      path: msg.path,
-      type: msg.type,
-      sender: 'contents'}
-  );
+
+for (const [key, val] of Object.entries(functionObj[msg.name])) {
+  if (key !== 'mouse' && typeof val === 'function') {
+  val();
+  } else if( typeof val === 'object') { 
+  executeEvents(val, msg.name);
+  }
+	console.log(key, val);
+}
+	
 
 });
