f3d39e521f1197a38805ee30fef0c719065979d6
jacobkim9881 <jacobkim9881@gmail.com>
2021-12-15 11:21:36 +0900

    test: functionized execute functions and splited into obj.js

--- contents.js
+++ contents.js
@@ -1,37 +1,20 @@
 localStorage.mouseCounter = 0;
+/*
 chrome.runtime.sendMessage(undefined,
   { type : 'check',
     sender: 'contents'}
 );
+*/
 console.log('contents')    
 
-function executeEvents(obj, name) {
-localStorage.pointerName = name;	
- for (const [key, func] of Object.entries(obj)) {
-    document.body.addEventListener(key, func);
-console.log(func)
-	 console.log(name)
-	 console.log(key)
-    chrome.runtime.onMessage.addListener((msg) => {
-	    console.log(msg)
-     msg.name !== name ? document.body.removeEventListener(key, func) : null;
-    });
- }
+console.log(functionObj[localStorage.pointerName])
+console.log(localStorage.pointerName)
 
-}
-
-executeEvents(functionObj[localStorage.pointerName], localStorage.pointerName);
+executeObjFunc(localStorage.pointerName)
 
 chrome.runtime.onMessage.addListener((msg, _, sendRes) => {
-
-for (const [key, val] of Object.entries(functionObj[msg.name])) {
-  if (key !== 'mouse' && typeof val === 'function') {
-  val();
-  } else if( typeof val === 'object') { 
-  executeEvents(val, msg.name);
-  }
-	console.log(key, val);
-}
+console.log(msg)
+executeObjFunc(msg.name)
 	
 
 });
--- obj.js
+++ obj.js
@@ -1,5 +1,32 @@
 let functionObj = {};
 
+function executeEvents(obj, name) {
+localStorage.pointerName = name;	
+ for (const [key, func] of Object.entries(obj)) {
+    document.body.addEventListener(key, func);
+console.log(func)
+	 console.log(name)
+	 console.log(key)
+    chrome.runtime.onMessage.addListener((msg) => {
+	    console.log(msg)
+     msg.name !== name ? document.body.removeEventListener(key, func) : null;
+    });
+ }
+
+}
+
+function executeObjFunc(msgName) {
+for (const [key, val] of Object.entries(functionObj[msgName])) {
+  if (key !== 'mouse' && typeof val === 'function') {
+  val();
+  } else if( typeof val === 'object') { 
+  executeEvents(val, msgName);
+  }
+	console.log(key, val);
+}
+
+}
+
 functionObj.bubble = {};
 
 functionObj.bubble.mouse = {}
