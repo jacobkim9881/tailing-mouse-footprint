8d7ba5a036ce489a43a2e6477b9da0573e83e0c4
jacobkim9881 <jacobkim9881@gmail.com>
2022-05-11 15:31:32 +0900

    test: new animation test

--- functions/test.js
+++ functions/test.js
@@ -1,76 +1,108 @@
-function mouseEvent(e) {
+//colofulBall.js
+  function trigger(e, ranH) {
+  let obj = document.createElement('div');  	  
+  let ballSize = window.innerWidth/100;
+  let randomBallSize = Math.trunc(Math.random() * ballSize);
+  let ranXpos = parseInt(e.clientX, 10);
+  let ranYpos = parseInt(e.clientY, 10);
+  let yHeight = parseInt(window.innerHeight);	  
+  let awayFromCursor = - 30 * (yHeight - ranYpos) /yHeight;
+//  let ranH = Math.trunc(Math.random() * 360);
+  obj.className = 'flubbers'	  
+  obj.style.position = 'fixed';
+  obj.style.left = ranXpos + 'px';
+  obj.style.width = ballSize + 'px';
+  obj.style.height = ballSize + 'px';
+  obj.style.backgroundColor = `hsl(${ranH}, 100%, 50%)`;
+  obj.style.borderRadius = '50%';
+obj.style.top= ranYpos + 'px';
+//https://codepen.io/rachelnabors/pen/eJyWzm/?editors=0010
+  let newKeyframe = new KeyframeEffect(obj, [
+    {top: (ranYpos * 0.96 + awayFromCursor) + 'px',
+	    transform: 'scale(1,1)',
+	    width: (ballSize * 1.1) + 'px',
+	    borderRadius: '70% 70% 50% 50%',
+	    opacity: '1',
+     backgroundColor: `hsl(${ranH}, 100%, 50%)`},
+    {top: (ranYpos * 0.8 )+ 'px',
+    transform: 'scale(0.95, 0.95)',
+	    width: (ballSize * 0.95) + 'px',
+	    borderRadius: '50% 50% 50% 50%',
+	    opacity: '1',
 
-  let xSize = 186;
-  let squareWid = Math.random() * 0.4 + 0.1;
-  let pOrM = Math.random() >= 0.5 ? 1 : -1;
+    backgroundColor: `hsl(${ranH + 10}, 100%, 50%)`},
+    {top: (ranYpos * 0.6) + 'px',
+    transform: 'scale(1,1)',
+	    width: (ballSize * 1.05) + 'px',
+ borderRadius: '60% 60% 50% 50%',
+	    opacity: '0.8',
 
-  function trigger(e) {
-    let obj = document.createElement('div');
-    let ranXpos = parseInt(e.clientX, 10);
-    let ranYpos = parseInt(e.clientY, 10);
-    obj.style.position = 'fixed';
-    obj.style.width = (window.innerWidth/60) + 'px';
-    obj.style.height = (window.innerHeight*1/300) + 'px';
-    obj.style.backgroundColor = `hsl(${(localStorage.mouseCounter * 10) % 360}, 100%, 50%)`;
-    //obj.style.borderRadius = '50%';
-  
-    document.body.appendChild(obj);
-    setTimeout(() => obj.remove(), xSize * 2 * (300/xSize));
-    return obj;
+    backgroundColor: `hsl(${ranH + 20}, 100%, 50%)`},
+    {top: (ranYpos * 0.3) + 'px',
+    transform: 'scale(0.95, 0.95)',
+    width: (ballSize * 1) + 'px',
+ borderRadius: '50% 50% 50% 50%',
+	    opacity: '0.3',
+
+    backgroundColor: `hsl(${ranH + 30}, 100%, 50%)`},
+  ], 5000)
+ 
+  let flubAni = new Animation(newKeyframe, document.timeline); 
+
+  document.body.appendChild(obj);
+  setTimeout(() => obj.remove(), 4900);
+  return;
   }
+
+function mouseEvent(e) {
+//let ranH = num %  360;
+
+        localStorage.yMousePos = e.clientY;	
+        localStorage.xMousePos = e.clientX;	
   let num = parseInt(localStorage.mouseCounter);
   localStorage.mouseCounter = num + 1;
-  if (num % 3 === 0 ) {  
-    let barLen = window.innerWidth/40  
-    , ballPos = {x: e.clientX , y: e.clientY}  
-    , obj = trigger(e)
-    , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1;	     
+  let ranH = num %  360;
+ localStorage.mouseLazy  
+  if (num %  7 === 0 ) {
+   trigger(e, ranH);
 
-    for (let i = - xSize; i <= xSize; i++) {     
-      let i2 = i >= 0 ? i - 180 : i;	   
-      setTimeout(() => {
-        let newX = ballPos.x - (i2/xSize) * roDeg * (barLen) * 0.2
-        , newY = ballPos.y - 10 + (Math.sin(i2/xSize * Math.PI) * barLen);
-        obj.style.left = newX + 'px';
-        obj.style.top = newY + 'px';        
-        obj.style.transform = `rotate(${i2 * roDeg}deg)`;	   
-        localStorage.xMousePos = e.clientX;	
-      }, (xSize + 1 + i) * (300/xSize))
-    }
   }
 }
-function setObjs() {
-  let oClass = {
-      class: 'objs-test',
-      width: '15px',
-      height: '45px',
-      border: '1px solid white',	  
-      backgroundColor : 'none',
-      top: (window.innerHeight -100) + 'px'
-    },
-    cntWidth = Math.trunc(window.innerWidth / 45);
-  for (let i = 0; i < cntWidth; i++) {
-    let oneObj = document.createElement('div');
-    oneObj.id = oClass.class + i;
-    oneObj.className = 'off';	  
-    oneObj.style.width = oClass.width;
-    oneObj.style.height = oClass.height;
-    //    oneObj.style.backgroundColor = oClass.backgroundColor;
-    oneObj.style.top = oClass.top;
-    oneObj.style.left = 50 + i * 45 + 'px';
-    oneObj.style.position = 'fixed';
-    oneObj.style.border = oClass.border;	
-    document.body.appendChild(oneObj);	  
+
+ setInterval(() => {
+    let allObj = document.getElementsByClassName('flubbers')
+  if ( localStorage.fYMousePos = localStorage.yMousePos
+&& allObj.length > 0 ) {
+//OA	  console.log(allObj)
+ for (let i = 0; i < allObj.length; i++){
+ let obj = allObj[i]	
+ , ranYpos = localStorage.fYMousePos 	 
+      obj.animate([
+    {top: (ranYpos ) + 'px',
+    transform: 'scale(0.95, 0.95)',
+ borderRadius: '50% 50% 50% 50%',
+},
+    {top: (ranYpos * 1.2 )+ 'px',
+    transform: 'scale(0.95, 0.95)',
+	    borderRadius: '50% 50% 50% 50%',
+},
+    {top: (ranYpos * 1.4) + 'px',
+    transform: 'scale(1,1)',
+ borderRadius: '60% 60% 50% 50%',
+},
+
+
+  ], 5000)	    
+    }	  
   }
-}
-function deleteObjs() {
-  let cntWidth = Math.trunc(window.innerWidth / 45);
-  for (let i = 0; i < cntWidth; i++) {
-    if (!document.getElementById(`objs-test${i}`)) {continue;}
-    document.getElementById(`objs-test${i}`).remove();
-  }
-}
+  localStorage.fYMousePos = localStorage.yMousePos  
+  localStorage.fXMousePos = localStorage.xMousePos 
+
+  localStorage.fYMousePos = localStorage.fYMousePos + 1  
+},50)
+
 document.body.addEventListener('mousemove', mouseEvent);
+
 chrome.runtime.onMessage.addListener((msg) => {
   document.body.removeEventListener('mousemove', mouseEvent);
 });
