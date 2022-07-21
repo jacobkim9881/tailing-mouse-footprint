fd9c7d5a3c43947902f189fe8bf22f5be93d5624
jacobkim9881 <jacobkim9881@gmail.com>
2022-07-22 00:11:14 +0900

    test: loop funtions

--- functions/test.js
+++ functions/test.js
@@ -1,36 +1,80 @@
-//test.js
 function mouseEvent(e) {
-// object for styling
-  let obj = document.createElement('div');
-  
-	
-  let ran10 = Math.trunc(Math.random() * 10) + 5;
-    let ranXpos = Math.trunc(Math.random() * ran10) + parseInt(e.clientX, 10);
-    let ranYpos = Math.trunc(Math.random() * 10) + parseInt(e.clientY, 10);
-    let ran20 = ranYpos - 30;
-    obj.style.position = 'fixed';
-    obj.style.backgroundImage = `linear-gradient(to bottom right, hsl(170, 100%, 50%), hsl(170, 100%, 0%)`;
-    obj.style.borderRadius = '50%';  
-    obj.animate([
-      {top: ranYpos + 'px',
-      left: e.clientX + 'px',
-      width: 5 + 'px',
-      height: 5 + 'px' 
-    }, 
-      {top: ran20 + 'px',
-      left: ranXpos + 'px',
-      width: ran10 + 'px',
-      height: ran10 + 'px' 
-    }
-    ], 200)
 
-  document.body.appendChild(obj);
-  setTimeout(() => obj.remove(), 200);
-  return;
+	  let xSize = 200;
+	  let squareWid = Math.random() * 0.4 + 0.1;
+	  let pOrM = Math.random() >= 0.5 ? 1 : -1;
+
+	  function trigger(e) {
+		      let obj = document.createElement('div');
+		      let ballSize = window.innerWidth/150;
+		      let randomBallSize = Math.trunc(Math.random() * ballSize) + 5;
+		      let ranXpos = parseInt(e.clientX, 10);
+		      let ranYpos = parseInt(e.clientY, 10);
+		      let ranH = Math.trunc(Math.random() * 360);
+		      let ranH2 =  Math.trunc(Math.random() * 360); 
+		      obj.style.position = 'fixed';
+		      obj.style.left = ranXpos + 'px';
+		      obj.style.top = ranYpos + 'px'
+		      obj.style.width = randomBallSize + 'px';
+		      obj.style.height = randomBallSize + 'px';
+		      obj.style.backgroundColor = `hsl(${ranH}, 100%, 50%)`;
+		      obj.style.borderRadius = '50%';
+		    
+		      document.body.appendChild(obj);
+		      setTimeout(() => obj.remove(), xSize * 2 * (600/xSize));
+		      return obj;
+		    }
+	  let num = parseInt(localStorage.mouseCounter);
+	  localStorage.mouseCounter = num + 1;
+	  if (num %  3 === 0 ) {  
+		      let ballRad = 100;
+		      let ballPos = {x: e.clientX + ballRad , y: e.clientY};  
+		      let obj = trigger(e);
+		      for (let i = -xSize; i <= xSize; i++) {     
+			            let t = i >= 0 ? 100 + i : 100 + i;
+			            setTimeout(() => {
+
+					            let newX = pOrM === 1 ? i / 2 + ballPos.x : i * pOrM / 2 - xSize + ballPos.x;
+					            let newY;             
+					            newY = Math.pow(i, 2)/(100/squareWid) - Math.pow(-xSize, 2)/(100/squareWid) + ballPos.y;       
+
+					            obj.style.left = newX + 'px';
+					            obj.style.top = newY + 'px';        
+					          }, (xSize + 1 + i) * (600/xSize))
+			          }
+		    }
 }
-
+function setObjs() {
+	  let oClass = {
+		        class: 'objs-test',
+		        width: '15px',
+		        height: '45px',
+		        border: '1px solid white',	  
+		        backgroundColor : 'none',
+		        top: (window.innerHeight -100) + 'px'
+		      },
+		    cntWidth = Math.trunc(window.innerWidth / 45);
+	  for (let i = 0; i < cntWidth; i++) {
+		      let oneObj = document.createElement('div');
+		      oneObj.id = oClass.class + i;
+		      oneObj.className = 'off';	  
+		      oneObj.style.width = oClass.width;
+		      oneObj.style.height = oClass.height;
+		      oneObj.style.top = oClass.top;
+		      oneObj.style.left = 50 + i * 45 + 'px';
+		      oneObj.style.position = 'fixed';
+		      oneObj.style.border = oClass.border;	
+		      document.body.appendChild(oneObj);	  
+		    }
+}
+function deleteObjs() {
+	  let cntWidth = Math.trunc(window.innerWidth / 45);
+	  for (let i = 0; i < cntWidth; i++) {
+		      if (!document.getElementById(`objs-test${i}`)) {continue;}
+		      document.getElementById(`objs-test${i}`).remove();
+		    }
+}
 document.body.addEventListener('mousemove', mouseEvent);
-
 chrome.runtime.onMessage.addListener((msg) => {
-document.body.removeEventListener('mousemove', mouseEvent)
+	  document.body.removeEventListener('mousemove', mouseEvent);
 });
