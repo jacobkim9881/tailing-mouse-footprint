e64e8f6fab73d1552fe0942067ae4ef3602bb584
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-12 18:19:31 +0900

    test: added limit and star moves

--- functions/test.js
+++ functions/test.js
@@ -42,7 +42,8 @@ function trigger(e, direction) {
   }
 
 function mouseEvent(e) {
-  let xSize = Math.trunc(Math.random() * 50) + 35;
+  let xSize = 200
+		//Math.trunc(Math.random() * 50) + 35;
   let squareWid = Math.random() * 0.4 + 0.1;
   let pOrM = Math.random() >= 0.5 ? 1 : -1;
    let num = parseInt(localStorage.mouseCounter);
@@ -50,44 +51,53 @@ function mouseEvent(e) {
   if (num %  13 === 0 ) {
   let  ballPos = {x: e.clientX , y: e.clientY}  
    , ran50 = Math.trunc(Math.random() * 150) * pOrM + 15
-   , ranYpos = Math.trunc(Math.random() * ran50) + parseInt(e.clientY, 10)
+   , ranYpos = Math.trunc(Math.random() * ran50) + parseInt(e.clientY, 10) 
       , obj1 = trigger(e)
       , obj2 = trigger(e)	  
-      , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1;	          
+      , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1	 
+	, limit = 200  
+     , edge = roDeg === 1 ? ballPos.x + limit : ballPos.x - limit 
     obj1.style.top = ranYpos + 'px'
     obj2.style.top = ranYpos + 'px'
 	  console.log('roDeg: ', roDeg)
  for (let i = 0; i <= xSize; i++) {     
-      //let i2 = i >= 0 ? i - 180 : i;	   
+//	 if (i === Math.abs(ran50)) break;	 	 
+      //let i2 = i >= 0 ? i - 180 : i;	  
       setTimeout(() => {
         let obj1X , obj2X, width1
 	if (roDeg === -1) {      
-        obj1X = ran50 + ballPos.x - i 
-        , obj2X = ran50 + ballPos.x - i - 2
+        obj1X = ran50 + ballPos.x - 2 * i 
+        , obj2X = ran50 + ballPos.x - 2 * i - 2
 	, width1 = i + 1  
 //	      console.log('obj1x: ', obj1X)
         obj1.style.left = obj1X + 'px';
         obj2.style.left = obj2X + 'px';
+        if(obj2X < edge) return
+	      //console.log(obj2X < edge)
 	} else {
-        obj1X = ran50 + ballPos.x  
-        , obj2X = ran50 + ballPos.x + i + 1 
+        obj1X = ran50 + ballPos.x + 2 * i 
+        , obj2X = ran50 + ballPos.x + 2 * i + 1 
 	, width1 = i + 1  
 //	      console.log('obj1x: ', obj1X)
         obj1.style.left = obj1X + 'px';
         obj2.style.left = obj2X + 'px';
+        if(obj2X > edge) return
+	      //console.log(obj2X > edge)
 	}
         obj1.style.width = width1 + 'px';
-        obj2.style.width = 1 + 'px';
-  
+        obj2.style.width = 1 + 'px';  	
   document.body.appendChild(obj1);
   document.body.appendChild(obj2);
-  setTimeout(() =>  {
-	  obj1.remove()
-	  obj2.remove()
+	      //console.log('obj2X: ', obj2X)
+	      //console.log('edge: ', edge)
+     setTimeout(() =>  {
+	 obj1.remove()
+	 obj2.remove()
         localStorage.xMousePos = e.clientX;	
-  } ,(xSize + 1 + i + 200));
+  } ,(400));
       }, (xSize + 1 + i))
     }
+
   } 
 }
 
