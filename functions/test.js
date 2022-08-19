eeb33691eea0f0d0d5d31a3f29aca18d32f67e7a
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-20 01:50:48 +0900

    test: functionized and add more obj

--- functions/test.js
+++ functions/test.js
@@ -9,28 +9,25 @@ function trigger(e) {
   return obj;
   }
 
-function mouseEvent(e) {
-  let xSize = 100
-		//Math.trunc(Math.random() * 50) + 35;
-  let squareWid = Math.random() * 0.4 + 0.1;
-  let pOrM = Math.random() >= 0.5 ? 1 : -1;
-   let num = parseInt(localStorage.mouseCounter);
-  localStorage.mouseCounter = num + 1;
-  if (num %  13 === 0 ) {
-  let  ballPos = {x: e.clientX , y: e.clientY}  
-      , obj1 = trigger(e)
+function buildObj(e, roDeg, pOrM, xSize, ballPos, edge) {
+   if (Math.random() > 0.5) return;
+   const obj1 = trigger(e)
       , obj2 = trigger(e)	  
-      , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1	 
-   , ran50 = (Math.trunc(Math.random() * 50) + 15) * roDeg
-   , ranYpos = Math.trunc(Math.random() * 50 + 30) * pOrM + parseInt(e.clientY, 10) 
-	, limit = 200 
-     , edge = roDeg === 1 ? ballPos.x + limit : ballPos.x - limit 
+   , ran50 = (Math.trunc(Math.random() * 150) + 15) * pOrM 
+   , ranYpos = Math.trunc(Math.random() * 80) * pOrM + parseInt(e.clientY, 10) 
     obj1.style.top = ranYpos + 'px'
     obj2.style.top = ranYpos + 'px'
-	  console.log('roDeg: ', roDeg)
+	 // console.log('roDeg: ', roDeg)
  for (let i = 0; i <= xSize; i++) {     
 //	 if (i === Math.abs(ran50)) break;	 	 
       //let i2 = i >= 0 ? i - 180 : i;	  
+loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize)
+    }
+
+
+}
+
+function loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
       setTimeout(() => {
         let obj1X , obj2X, width1
 	if (roDeg === -1) {      
@@ -65,9 +62,27 @@ function mouseEvent(e) {
 	 obj2.remove()
         localStorage.xMousePos = e.clientX;	
   } ,(300));
-    }
+}
 
-  } 
+function mouseEvent(e) {
+  let xSize = 100
+		//Math.trunc(Math.random() * 50) + 35;
+  let squareWid = Math.random() * 0.4 + 0.1;
+  let pOrM = Math.random() >= 0.5 ? 1 : -1;
+   let num = parseInt(localStorage.mouseCounter);
+  localStorage.mouseCounter = num + 1;
+  if (num %  13 === 0 ) {
+  let  ballPos = {x: e.clientX , y: e.clientY}  
+      , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1	  
+	, limit = 200 
+     , edge = roDeg === 1 ? ballPos.x + limit : ballPos.x - limit 
+
+	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
+	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
+	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
+	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
+	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
+	    } 
 }
 
 document.body.addEventListener('mousemove', mouseEvent);
