376a3a30b6a9c733110125ce24ee8929e9146b1d
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-24 02:08:25 +0900

    test: longer star respawn

--- functions/test.js
+++ functions/test.js
@@ -30,19 +30,18 @@ loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize)
 function loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
       setTimeout(() => {
         let obj1X , obj2X, width1
+	width1 = i < 50 ? i + 1 : 50 
 	if (roDeg === -1) {      
-        obj1X = ran50 + ballPos.x - 2 * i 
-        , obj2X = ran50 + ballPos.x - 2 * i - 2
-	, width1 = i + 1  
+        obj1X =  ballPos.x - 2 * (i * i)/10 
+        , obj2X =  ballPos.x - 2 * (i * i)/10 - 2
 //	      console.log('obj1x: ', obj1X)
         obj1.style.left = obj1X + 'px';
         obj2.style.left = obj2X + 'px';
         if(obj2X < edge) return
 	      //console.log(obj2X < edge)
 	} else {
-        obj1X = ran50 + ballPos.x + 2 * i 
-        , obj2X = ran50 + ballPos.x + 2 * i + 1 
-	, width1 = i + 1  
+        obj1X =  ballPos.x + 2 * (i * i)/10 
+        , obj2X =  ballPos.x + 2 * (i * i)/10 + 1 
 //	      console.log('obj1x: ', obj1X)
         obj1.style.left = obj1X + 'px';
         obj2.style.left = obj2X + 'px';
@@ -52,7 +51,7 @@ function loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
         obj1.style.width = width1 + 'px';
         obj2.style.width = 1 + 'px';  	
 
-      }, (xSize + 1 + i)) 
+      }, (xSize + 1 + i * 10)) 
 	 document.body.appendChild(obj1);
   document.body.appendChild(obj2);
 	      //console.log('obj2X: ', obj2X)
@@ -61,7 +60,7 @@ function loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
 	 obj1.remove()
 	 obj2.remove()
         localStorage.xMousePos = e.clientX;	
-  } ,(300));
+  } ,(1000));
 }
 
 function mouseEvent(e) {
@@ -71,16 +70,12 @@ function mouseEvent(e) {
   let pOrM = Math.random() >= 0.5 ? 1 : -1;
    let num = parseInt(localStorage.mouseCounter);
   localStorage.mouseCounter = num + 1;
-  if (num %  13 === 0 ) {
+  if (num %  3 === 0 ) {
   let  ballPos = {x: e.clientX , y: e.clientY}  
       , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1	  
 	, limit = 200 
      , edge = roDeg === 1 ? ballPos.x + limit : ballPos.x - limit 
 
-	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
-	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
-	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
-	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
 	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
 	    } 
 }
