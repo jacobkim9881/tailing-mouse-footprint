f9ea62efdfb537f8c29863b3978dea4a495355de
jacobkim9881 <jacobkim9881@gmail.com>
2022-08-25 03:50:00 +0900

    test: when mouse move difference under 50 then star dimming

--- functions/test.js
+++ functions/test.js
@@ -9,7 +9,7 @@ function trigger(e) {
   return obj;
   }
 
-function buildObj(e, roDeg, pOrM, xSize, ballPos, edge) {
+function buildObj(e, roDeg, pOrM, xSize, ballPos, edge, func) {
    if (Math.random() > 0.5) return;
    const obj1 = trigger(e)
       , obj2 = trigger(e)	  
@@ -21,10 +21,36 @@ function buildObj(e, roDeg, pOrM, xSize, ballPos, edge) {
  for (let i = 0; i <= xSize; i++) {     
 //	 if (i === Math.abs(ran50)) break;	 	 
       //let i2 = i >= 0 ? i - 180 : i;	  
-loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize)
+func(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize)
     }
 
 
+}
+
+function dimmingStar(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
+      setTimeout(() => {
+        let obj1X , obj2X, width1
+	width1 = i < 50 ? i + 1 : 50 
+	, rightAndLeft = i % 2      
+        obj1X =  ballPos.x - 4 * rightAndLeft + 2 + ran50 
+        , obj2X =  ballPos.x - 4 * rightAndLeft + 2 + ran50
+			      console.log('obj1x: ', obj1X)
+	      console.log('obj2X: ', obj2X)
+        obj1.style.left = obj1X + 'px';
+        obj2.style.left = obj2X + 'px';
+	      console.log(obj2X < edge)
+	      obj1.style.width = 1 + 'px';
+        obj2.style.width = 1 + 'px';  	
+
+      }, (xSize + 1 + i * 10)) 
+	 document.body.appendChild(obj1);
+//  document.body.appendChild(obj2);
+	      //console.log('edge: ', edge)
+     setTimeout(() =>  {
+	 obj1.remove()
+	 //obj2.remove()
+        localStorage.xMousePos = e.clientX;	
+  } ,(1000));
 }
 
 function loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
@@ -32,16 +58,16 @@ function loopObj(obj1, obj2, roDeg, ballPos, i, ran50, edge, e, xSize) {
         let obj1X , obj2X, width1
 	width1 = i < 50 ? i + 1 : 50 
 	if (roDeg === -1) {      
-        obj1X =  ballPos.x - 2 * (i * i)/10 
-        , obj2X =  ballPos.x - 2 * (i * i)/10 - 2
+        obj1X =  ballPos.x - 2 * (i * i)/10 - (ran50 - i) 
+        , obj2X =  ballPos.x - 2 * (i * i)/10 - 2- (ran50 - i) 
 //	      console.log('obj1x: ', obj1X)
         obj1.style.left = obj1X + 'px';
         obj2.style.left = obj2X + 'px';
         if(obj2X < edge) return
 	      //console.log(obj2X < edge)
 	} else {
-        obj1X =  ballPos.x + 2 * (i * i)/10 
-        , obj2X =  ballPos.x + 2 * (i * i)/10 + 1 
+        obj1X =  ballPos.x + 2 * (i * i)/10 + (ran50 - i) 
+        , obj2X =  ballPos.x + 2 * (i * i)/10 + 1 + (ran50 - i) 
 //	      console.log('obj1x: ', obj1X)
         obj1.style.left = obj1X + 'px';
         obj2.style.left = obj2X + 'px';
@@ -71,12 +97,15 @@ function mouseEvent(e) {
    let num = parseInt(localStorage.mouseCounter);
   localStorage.mouseCounter = num + 1;
   if (num %  3 === 0 ) {
-  let  ballPos = {x: e.clientX , y: e.clientY}  
+  let  ballPos = {x: e.clientX , y: e.clientY} 
+	, diff =  e.clientX - localStorage.xMousePos 
       , roDeg = e.clientX - localStorage.xMousePos > 0 ? -1 : 1	  
 	, limit = 200 
      , edge = roDeg === 1 ? ballPos.x + limit : ballPos.x - limit 
+//console.log(diff)
+    if (Math.abs(diff) > 50)  buildObj(e, roDeg, pOrM, xSize, ballPos, edge, loopObj) 
+	  else  buildObj(e, roDeg, pOrM, xSize, ballPos, edge, dimmingStar);
 
-	  buildObj(e, roDeg, pOrM, xSize, ballPos, edge) 
 	    } 
 }
 
